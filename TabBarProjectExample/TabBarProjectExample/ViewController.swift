aef943c40993924a17d8ff129ba055ec24d70d61
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-01-11 08:27:16 -0500

    Added Project in the repository

---
+++ TabBarProjectExample/TabBarProjectExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarProjectExample
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 11/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
